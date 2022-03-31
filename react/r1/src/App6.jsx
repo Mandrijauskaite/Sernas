7ddbca5dae71908f7e6e5187de3be49d33769658
Mandrijauskaite <97032997+Mandrijauskaite@users.noreply.github.com>
2022-03-31 16:12:47 +0300

    change

--- react/r1/src/App6.jsx
+++ react/r1/src/App6.jsx
@@ -1,19 +1,14 @@
 import './App.css';
-import Kvadratas from './Components/Kvadratas';
-import Medziai from './Components/Medziai';
-import Myg from './Components/Myg';
-import Mygtukas1 from './Components/Mygtukas1';
-import Mygtukai from './Mygtukai';
-
+import MelynasisKvadratelis from './Components/MelynasisKvadratelis';
+import Pasaka from './Components/Pasaka';
 
 function App() {
+
   return (
     <div className="App">
-        <Mygtukas1 size="600px"></Mygtukas1>
-        <Medziai color="black"></Medziai>
-        {/* <Myg></Myg> */}
-        <Mygtukai></Mygtukai>
-        {/* <Kvadratas></Kvadratas> */}
+        <h1>HOOK</h1>
+        <Pasaka></Pasaka>
+        <MelynasisKvadratelis></MelynasisKvadratelis>
     </div>
   );
 }
